3546f72087b429ea537af51633e4ec521b96461d
Noushadozi <noushadozi333@gmail.com>
2023-09-25 16:12:05 +0600

    nab var responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link, NavLink } from 'react-router-dom';
+import { RxCross1 } from 'react-icons/rx';
+import { FiAlignCenter } from 'react-icons/fi';
 
 const Header = () => {
+    const [nav, setNav] = useState(true);
+
+    const handleResponsiveNav = () => {
+        setNav(!nav);
+    }
+
     return (
         <nav>
-            <NavLink className='nav-home-btn' to="/">DownDal</NavLink>
-            <div className='nav-btn-group'>
-                <NavLink className='nav-btn' to="/statistics">Statistics</NavLink>
-                <NavLink className='nav-btn' to="/jobs">Applied Jobs</NavLink>
+            <div onClick={handleResponsiveNav} className='nav-responsive-icons'>
+                {
+                    nav ?
+                        <FiAlignCenter style={{padding: '18px'}}/>
+                        : <RxCross1  style={{padding: '18px'}}/>
+                }
             </div>
-            <Link><button to="/" className='nav-apply-btn'>Star Applying</button></Link>
+            <div className={`nav-items ${nav ? "hidden" : "visible"}`} >
+                <NavLink className='nav-home-btn a' to="/">DownDal</NavLink>
+                <div className='nav-btn-group'>
+                    <NavLink className='nav-btn a' to="/statistics">Statistics</NavLink>
+                    <NavLink className='nav-btn a' to="/jobs">Applied Jobs</NavLink>
+                </div>
+                <Link><button to="/" className='nav-apply-btn'>Star Applying</button></Link>
+            </div>
         </nav>
     );
 };
